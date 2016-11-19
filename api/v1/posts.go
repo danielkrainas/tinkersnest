159af3c9878341922ef7b3c7ebe14dadb1182901
Danny Krainas <dkrainas87@gmail.com>
2016-11-19 00:05:54 -0500

    update example object specs

---
+++ api/v1/posts.go
@@ -0,0 +1,22 @@
+package v1
+
+type Post struct {
+	Name    string     `json:"name" yaml:"name"`
+	Title   string     `json:"title" yaml:"title"`
+	Publish bool       `json:"publish" yaml:"publish"`
+	Created int64      `json:"created" yaml:"created"`
+	Content []*Content `json:"content" yaml:"content"`
+}
+
+type ContentType string
+
+var (
+	ContentMarkdown ContentType = "markdown"
+	ContentText     ContentType = "text"
+	ContentHtml     ContentType = "html"
+)
+
+type Content struct {
+	Type string `json:"type" yaml:"type"`
+	Data []byte `json:"data" yaml:"data"`
+}
