c66950dede79016212d9eb311034750ef046ce65
Danny Krainas <dkrainas87@gmail.com>
2016-11-19 00:05:41 -0500

    drop the create post object and make posts model more flexible

--- api/v1/models.go
+++ api/v1/models.go
@@ -5,20 +5,6 @@ import (
 	"net/http"
 )
 
-type CreatePostRequest struct {
-	Title string `json:"title"`
-	Slug  string `json:"slug"`
-	Body  string `json:"body"`
-}
-
-type Post struct {
-	ID      string `json:"id"`
-	Title   string `json:"title"`
-	Slug    string `json:"slug"`
-	Body    string `json:"body"`
-	Created int64  `json:"created"`
-}
-
 func ServeJSON(w http.ResponseWriter, data interface{}) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
