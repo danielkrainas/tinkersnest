6e95c0e398884875990fccb980fbe26edd8aa5ec
Danny Krainas <dkrainas87@gmail.com>
2016-11-22 22:32:12 -0500

    fix user auth

--- api/server/handlers/auth.go
+++ api/server/handlers/auth.go
@@ -64,7 +64,7 @@ func (ctx *authHandler) Auth(w http.ResponseWriter, r *http.Request) {
 	acontext.GetLogger(ctx).Warnf("using %s", creds.Password)
 	acontext.GetLogger(ctx).Warnf("attempting %s", creds.HashedPassword)
 	acontext.GetLogger(ctx).Warnf("expecting %s", user.HashedPassword)
-	if creds.HashedPassword != creds.Password {
+	if creds.HashedPassword != user.HashedPassword {
 		acontext.GetLogger(ctx).Error("invalid username or password")
 		ctx.Context = acontext.AppendError(ctx, errcode.ErrorCodeUnknown.WithDetail("invalid username or password"))
 		return
